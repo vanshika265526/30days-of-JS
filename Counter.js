50f123d0ec8c3a097fb4cc55fb80dc17dd190d3c
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2025-11-20 21:40:59 +0530

    Create Counter.js

---
+++ Counter.js
@@ -0,0 +1,16 @@
+/*
+Given an integer n, return a counter function. 
+This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+*/
+function createCounter(n) {
+    return function() {
+        return n++;  // return current value, then increment
+    };
+}
+
+// Usage:
+const counter = createCounter(5);
+console.log(counter()); // 5
+console.log(counter()); // 6
+console.log(counter()); // 7
+
